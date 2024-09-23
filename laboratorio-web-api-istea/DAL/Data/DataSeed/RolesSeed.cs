72dce165edbacdb3cab18d9c667d936cc07eb990
Agustin <asantinaque@protonmail.com>
2024-09-22 23:05:36 -0300

    fixes on changing from .6 to .8

--- laboratorio-web-api-istea/DAL/Data/DataSeed/RolesSeed.cs
+++ laboratorio-web-api-istea/DAL/Data/DataSeed/RolesSeed.cs
@@ -1,3 +1,4 @@
+using laboratorio_web_api_istea.DAL.Enum;
 using laboratorio_web_api_istea.DAL.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
@@ -9,11 +10,11 @@ public class RolesSeed : IEntityTypeConfiguration<Role>
     public void Configure(EntityTypeBuilder<Role> builder)
     {
         builder.HasData(
-            new Role() { Id = 1, Descripcion = "Bartender" },
-            new Role() { Id = 2, Descripcion = "Cervecero" },
-            new Role() { Id = 3, Descripcion = "Cocinero" },
-            new Role() { Id = 4, Descripcion = "Mozo" },
-            new Role() { Id = 5, Descripcion = "Socio" }
+            new Role() { Id = 1, Descripcion = RolesUsuarioEnum.Bartender.ToString() },
+            new Role() { Id = 2, Descripcion = RolesUsuarioEnum.Cervecero.ToString() },
+            new Role() { Id = 3, Descripcion = RolesUsuarioEnum.Cocinero.ToString() },
+            new Role() { Id = 4, Descripcion = RolesUsuarioEnum.Mozo.ToString() },
+            new Role() { Id = 5, Descripcion = RolesUsuarioEnum.Socio.ToString() }
         );
     }
 }
---
+++ laboratorio-web-api-istea/DAL/Enum/RolesUsuarioEnum.cs
@@ -0,0 +1,10 @@
+namespace laboratorio_web_api_istea.DAL.Enum;
+
+public enum RolesUsuarioEnum
+{
+    Bartender,
+    Cervecero,
+    Cocinero,
+    Mozo,
+    Socio
+}
